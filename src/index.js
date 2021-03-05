c7d5fb84427fc16828c76f1456d073729de8b560
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2021-03-05 20:34:50 +0300

    index.js page changed.

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ProjectManagementProvider } from './Contexts/ProjectManagementContext';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ProjectManagementProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ProjectManagementProvider>,
   document.getElementById('root')
 );
 
