fa97ed9f70040a5ab00d8eae7e86556ccb67aebb
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2021-03-11 01:37:45 +0300

    project page updated.

--- src/Components/Project.js
+++ src/Components/Project.js
@@ -1,13 +1,19 @@
 import React from 'react'
 import '../App.css';
+import Tasks from './Tasks';
+import CreateTask from './CreateTask';
 
 function Project({ project }) {
     return (
         <div className="col-md-4 col-6 mb-4">
-            <div className="card test">
+            <div className="card project-card">
                 <div className="card-body">
                     <span>{project.name} </span>
+                    <span>{project.id}</span>
+
                     <div className="text-right">
+                        <Tasks tasks={project.tasks} />
+                        <CreateTask project={project} />
                         <button className="btn btn-sm mr-2" style={{ background: '#A7E8C5', color: 'white' }}>
                             Edit
                         </button>
